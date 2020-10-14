cfea768746516da7f59431678ed62788a77b0e5c
sanjaybxl <sanjaybxl@gmail.com>
2020-10-14 12:56:29 +0530

    new requirement added

--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -6,7 +6,9 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  removable = true
+  removable = true;
+  selectable = true;
+
   availableColors = [
     { name: 'none', color: '' },
     { name: 'Primary', color: 'primary' },
